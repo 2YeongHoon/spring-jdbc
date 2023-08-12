23ad411bc8ed2d2669d84edc3fb008adae0f8fd6
2Yeonghoon <djaakclsrn12@naver.com>
2023-08-12 22:11:51 +0900

    NEW: 업데이트, 삭제 테스트 추가

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,6 +2,7 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +13,22 @@ class MemberRepositoryV0Test {
   @Test
   void crud() throws SQLException {
     // save
-    Member member = new Member("memberV2", 10000);
+    Member member = new Member("memberV100", 10000);
     memberRepository.save(member);
 
     // findById
     Member findMember = memberRepository.findById(member.getMemberId());
     Assertions.assertThat(member).isEqualTo(findMember);
+
+    // update
+    memberRepository.update(member.getMemberId(), 20000);
+    Member updateMember = memberRepository.findById(member.getMemberId());
+    Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+    // delete
+    memberRepository.delete(member.getMemberId());
+    Assertions.assertThatThrownBy(() -> memberRepository.findById(member.getMemberId()))
+        .isInstanceOf(NoSuchElementException.class);
   }
 
 }
