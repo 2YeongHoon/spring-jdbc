b0341cff30288fd5416343fb79190a6f90261ebb
2Yeonghoon <djaakclsrn12@naver.com>
2023-08-15 16:40:36 +0900

    TST: hikaridatasoruce 커넥션 풀 테스트

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,14 +1,17 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Slf4j
 public class ConnectionTest {
 
+  @Test
   void dataSourceTest() throws SQLException {
     Connection connection1 = DriverManager.getConnection(
         ConnectionConst.URL,
@@ -25,6 +28,25 @@ public class ConnectionTest {
 
   }
 
+  @Test
+  void dataSourceConnectionPool() throws SQLException, InterruptedException {
+    HikariDataSource hp = new HikariDataSource();
+    hp.setJdbcUrl(ConnectionConst.URL);
+    hp.setUsername(ConnectionConst.USERNAME);
+    hp.setPassword(ConnectionConst.PASSWORD);
+    hp.setMaximumPoolSize(10);
+    hp.setPoolName("MyPool");
+
+    Connection connection1 = hp.getConnection();
+    Connection connection2 = hp.getConnection();
+
+    log.info("connection = {}, class = {}", connection1 , connection1.getClass());
+    log.info("connection = {}, class = {}", connection2 , connection2.getClass());
+
+    Thread.sleep(1000);
+  }
+
+  @Test
   void driverManagerDataSourceTest() throws SQLException {
     DriverManagerDataSource dataSource = new DriverManagerDataSource(
         ConnectionConst.URL,
