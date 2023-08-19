a17bdf96056673d7ad3b0f43f947a4ee1b7b02f3
2Yeonghoon <djaakclsrn12@naver.com>
2023-08-19 17:30:10 +0900

    NEW: 계좌이체 트랜잭션 테스트 추가

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,30 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import java.sql.SQLException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+  private final MemberRepositoryV1 memberRepository;
+
+  public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+    Member fromMember = memberRepository.findById(fromId);
+    Member toMember = memberRepository.findById(toId);
+
+    memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+    validation(toMember);
+    memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+  }
+
+  private void validation(Member member) {
+    if(member.getMemberId().equals("ex")){
+      throw new IllegalStateException("이체중 예외 발생");
+    }
+  }
+
+}
